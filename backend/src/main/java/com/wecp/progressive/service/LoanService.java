c9760b903c9db9f91b910a69aef44930d64efc2e
username <wecp-dummy@gmail.com>
2025-09-28 09:32:15 +0000

    Day 10 Task Done

--- backend/src/main/java/com/wecp/progressive/service/LoanService.java
+++ backend/src/main/java/com/wecp/progressive/service/LoanService.java
@@ -1,29 +1,37 @@
 package com.wecp.progressive.service;
 
 import com.wecp.progressive.entity.Loan;
+import com.wecp.progressive.repository.LoanRepository;
 
 import java.util.List;
 
-public class LoanService {
+import org.springframework.stereotype.Service;
 
+@Service
+public class LoanService {
+    private final LoanRepository loanRepository;
+
+    public LoanService(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
 
     public List<Loan> getAllLoans() {
-        return null;
+        return loanRepository.findAll();
     }
 
     public Loan getLoanById(Long id) {
-        return null;
+        return loanRepository.findById(id).orElse(null);
     }
 
     public Loan createLoan(Loan loan) {
-        return null;
+        return loanRepository.save(loan);
     }
 
     public void updateLoan(Loan loan) {
-
+        loanRepository.save(loan);
     }
 
     public void deleteLoan(Long id) {
-
+        loanRepository.deleteById(id);
     }
 }
