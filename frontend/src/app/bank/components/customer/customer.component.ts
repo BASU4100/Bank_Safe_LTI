d00f584cd551f207526e3926a04921842a7b1fcb
username <wecp-dummy@gmail.com>
2025-10-14 16:19:50 +0000

    Day 18 done

--- frontend/src/app/bank/components/customer/customer.component.ts
+++ frontend/src/app/bank/components/customer/customer.component.ts
@@ -1,5 +1,40 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CustomerTS } from "../../types/tstypes/Customerts";
+import { FormsModule } from "@angular/forms";
 
-export class CustomerComponent  {
+@Component({
+    selector: 'component-customer',
+    templateUrl: './customer.component.html',
+    styleUrls: ['./customer.component.scss']
+})
+export class CustomersComponent implements OnInit {
+    isFormSubmitted: boolean | undefined;
+    customerSuccess$: any;
+    customerError$: any;
+    customerForm!: FormGroup;
+    customer: CustomerTS;
 
-  
+    constructor(private formBuilder: FormBuilder) {}
+    
+    ngOnInit(): void {
+        this.customerForm = this.formBuilder.group({
+            name: ['', [Validators.required]],
+            email: ['', [Validators.required, Validators.email]],
+            username: ['', [Validators.required]],
+            password: ['', [Validators.required, Validators.pattern(/^\w.{8,}$/)]]
+        })
+    }
+
+    onSubmit(): void {
+        if (this.customerForm.valid) {
+            this.customerSuccess$ = 'customer submitted successfully.';
+            this.isFormSubmitted = true;
+            this.customer = new CustomerTS(this.customerForm.get('name')?.value, this.customerForm.get('email')?.value, this.customerForm.get('username')?.value, this.customerForm.get('password')?.value, '');
+        }
+        else {
+            this.customerError$ = 'All fields are required.'
+        }
+    }
+
 }
