7ad40015f69064319294ce2f3dff985ac5321bc8
username <wecp-dummy@gmail.com>
2025-10-14 18:16:28 +0000

    Day 20 partial Tests

--- frontend/src/app/bank/components/account/account.component.ts
+++ frontend/src/app/bank/components/account/account.component.ts
@@ -1,5 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { AccountTS } from "../../types/tstypes/Accountts";
+import { CommonModule } from "@angular/common";
 
-export class AccountComponent {
- 
+@Component({
+    selector: 'app-account',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.scss']
+})
+export class AccountComponent implements OnInit {
+    accountForm: FormGroup;
+    account: AccountTS;
 
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.accountForm = this.fb.group({
+            customerId: ['C01', [Validators.required]],
+            balance: [0, [Validators.required, Validators.min(0)]]
+        });
+    }
+
+    onSubmit(): void{
+        if (this.accountForm.valid) {
+            this.account = new AccountTS(this.accountForm.get('customerId')?.value, this.accountForm.get('balance')?.value);
+        }
+    }
 }
--- frontend/src/app/bank/components/transaction/transaction.component.ts
+++ frontend/src/app/bank/components/transaction/transaction.component.ts
@@ -1,4 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { TransactionTS } from "../../types/tstypes/Transactionts";
+import { Transaction } from "../../types/Transaction";
+import { FormsModule, ReactiveFormsModule, FormControl } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
-export class TransactionComponent {
- 
+@Component({
+    selector: 'app-transaction',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './transaction.component.html',
+    styleUrls: ['./transaction.component.scss']
+})
+export class TransactionComponent implements OnInit{
+    transactionForm: FormGroup;
+    transaction: TransactionTS;
+
+    constructor(private fb: FormBuilder) {}
+    
+    ngOnInit(): void {
+        this.transactionForm = this.fb.group({
+            accountId: ['', [Validators.required]],
+            transactionType: ['', [Validators.required]],
+            amount: [0, [Validators.required, Validators.min(0)]],
+            transactionDate: ['', [Validators.required]],
+            transactionId: [0, [Validators.required]]
+        });
+    }
+
+    onSubmit() {
+        this.transaction = new TransactionTS(this.transactionForm.get('accountId')?.value, this.transactionForm.get('amount')?.value, this.transactionForm.get('transactionDate')?.value, this.transactionForm.get('transactionId')?.value);
+    }
 }
