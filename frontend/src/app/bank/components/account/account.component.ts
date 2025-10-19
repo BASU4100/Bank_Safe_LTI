7444c9c7befec1c18d1f064bfe4e7b135f694b09
username <wecp-dummy@gmail.com>
2025-10-19 09:50:48 +0000

    Day 22 passed.

--- frontend/src/app/bank/components/account/account.component.ts
+++ frontend/src/app/bank/components/account/account.component.ts
@@ -1,33 +1,52 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
-import { AccountTS } from "../../types/tstypes/Accountts";
 import { CommonModule } from "@angular/common";
+import { BankService } from "../../services/bank.service";
+import { Customer } from "../../types/Customer";
+import { Account } from "../../types/Account";
 
 @Component({
     selector: 'app-account',
-    standalone: true,
-    imports: [CommonModule, ReactiveFormsModule],
+    // standalone: true,
+    // imports: [CommonModule, ReactiveFormsModule],
     templateUrl: './account.component.html',
     styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
     accountForm!: FormGroup;
-    account: AccountTS | undefined;
-
-    constructor(private fb: FormBuilder) {}
+    account: Account | undefined;
+    customers: Customer[];
+    errorMessage: string;
+    successMessage: string;
+    constructor(private formBuilder: FormBuilder, private banksService: BankService){}
 
     ngOnInit(): void {
-        this.accountForm = this.fb.group({
-            account_id: ['', [Validators.required]],
-            customer_id: ['', [Validators.required]],
+        this.accountForm = this.formBuilder.group({
+            // account_id: ['', [Validators.required]],
+            customer: ['', [Validators.required]],
             balance: ['', [Validators.required, Validators.min(0)]]
         });
-        this.account = new AccountTS("1", 1000.00, "1");
+        // this.account = new AccountTS("1", 1000.00, "1");
+        this.loadCustomers();
     }
 
-    onSubmit(): void{
+    onSubmit(): void {
         if (this.accountForm.valid) {
-            this.account = new AccountTS(this.accountForm.get('customer_id')?.value, this.accountForm.get('balance')?.value, this.accountForm.get('account_id')?.value);
+        //   this.account = new Account(this.accountForm.value);
+            this.banksService.addAccount(this.accountForm.value).subscribe(data =>
+            this.account=data
+            )
+            this.successMessage = 'Account created successfully';
+            this.errorMessage = '';
+        } else {
+            this.successMessage = '';
+            this.errorMessage = 'Please fill out all required fields correctly.';
         }
+    }
+
+    loadCustomers(): void {
+        // this.banksService.getAllCustomers().subscribe(data =>
+        //     this.customers=data
+        // )
     }
 }
--- frontend/src/app/bank/components/customer/customer.component.ts
+++ frontend/src/app/bank/components/customer/customer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { CustomerTS } from "../../types/tstypes/Customerts";
 import { FormsModule } from "@angular/forms";
+import { BankService } from "../../services/bank.service";
+import { Customer } from "../../types/Customer";
 
 @Component({
     selector: 'component-customer',
@@ -13,31 +14,46 @@ export class CustomersComponent implements OnInit {
     customerSuccess: string = '';
     customerError: string = '';
     customerForm!: FormGroup;
-    customers: CustomerTS[];
+    customers: Customer[] = [];
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder, private bankService: BankService) {}
     
     ngOnInit(): void {
         this.customerForm = this.formBuilder.group({
             name: ['', [Validators.required]],
             email: ['', [Validators.required, Validators.email]],
-            username: ['', [Validators.required]],
-            password: ['', [Validators.required, Validators.pattern(/^\w.{8,}$/)]]
+            username: ['', [Validators.required, this.noSpecialCharacters]],
+            password: ['', [Validators.required, Validators.pattern(/^\w.{8,}$/)]],
+            role: ['']
         })
     }
 
     private noSpecialCharacters(control: any): {[key: string]: boolean} | null {
-        return null;
+        if (control.value && /[\W_]/.test(control.value)) {
+            return { specialCharacters: true };
+          }
+          return null;
     }
 
     onSubmit(): void {
         if (this.customerForm.valid) {
-            this.customerSuccess = 'customer submitted successfully.';
-            this.isFormSubmitted = true;
-            this.customers.push(new CustomerTS(this.customerForm.get('name')?.value, this.customerForm.get('email')?.value, this.customerForm.get('username')?.value, this.customerForm.get('password')?.value, ''));
+            this.banksService.addCustomer(this.customerForm.value).subscribe({
+                next: (data) => {
+                  this.customerSuccess = 'Customer created successfully';
+                  this.customerError = '';
+                  this.customers.push(data)
+                  this.isFormSubmitted = true;
+                  this.customerForm.reset();
+                },
+                error: () => {
+                    this.customerError = 'Failed to create customer.';
+                    this.customerSuccess = '';
+                }
+            });
         }
         else {
-            this.customerError = 'All fields are required.'
+            this.customerError = 'Please fill out all required fields correctly.';
+            this.customerSuccess = '';
         }
     }
 
